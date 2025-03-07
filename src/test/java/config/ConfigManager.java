b0a4c57cc6b339428bd6c019c2a9beb310a92357
Prasanth Sankaran <prasanthsnkr@gmail.com>
2025-03-07 14:52:31 +0530

    Implement config file and ConfigManager class for value retrieval

---
+++ src/test/java/config/ConfigManager.java
@@ -0,0 +1,31 @@
+package config;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import javax.management.RuntimeErrorException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigManager {
+	
+ private static Properties properties = new Properties();
+     
+	 static {
+		 try {
+			FileInputStream file = new FileInputStream(new File("./src/test/resources/utils/Config.properties"));
+			properties.load(file);
+		} catch (IOException e) {
+			throw new RuntimeErrorException(null, "Failed to load configuration file due to " + e.getMessage());
+		}
+		 
+	 }
+	 
+	 public static String getProperty(String KeyName) {
+		 return properties.getProperty(KeyName);
+	 }
+
+}
